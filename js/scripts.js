20477bed254402e17b014d71d59a4fb98dab91f5
Clayton Wong <clayton.wong@ookla.com>
2019-07-04 09:00:57 -0700

    add build/deploy

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+    $("#current_year").text(new Date().getFullYear());
+});
